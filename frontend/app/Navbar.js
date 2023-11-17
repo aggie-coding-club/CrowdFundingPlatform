2172d81ac6ee71b8cf924c1ef6fa0d2b7704286b
RandilW <85533296+RandilW@users.noreply.github.com>
2023-11-17 15:08:18 -0600

    search bar

--- frontend/app/Navbar.js
+++ frontend/app/Navbar.js
@@ -7,6 +7,7 @@ import { Dialog } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 
 const navigation = [
+    { name: 'Home', href: '/' },
     { name: 'About', href: '/About' },
     { name: 'Campaigns', href: '/Campaigns' },
     { name: 'Contact', href: '/Contact' },
--- frontend/app/page.js
+++ frontend/app/page.js
@@ -2,10 +2,16 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="bg-white py-24">
-      <div>
-        <p>Homepage goes here!</p>
-      </div>
+    <main data-theme="light">
+        <div className="hero min-h-screen bg-base-100">
+            <div className="hero-content text-center">
+                <div className="max-w-md">
+                    <h1 className="text-5xl font-bold">CrowdFunding Platform</h1>
+                    <p className="py-6">description.....</p>
+                    <button className="btn">Campaigns</button>
+                </div>
+            </div>
+        </div>
     </main>
   )
 }
