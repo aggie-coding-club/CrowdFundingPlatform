398f9b32acce95911f69009be874cee7f9460020
RandilW <85533296+RandilW@users.noreply.github.com>
2023-11-17 16:39:38 -0600

    changed homepage

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,17 +1,69 @@
-import Image from 'next/image'
 
 export default function Home() {
-  return (
-    <main data-theme="light">
-        <div className="hero min-h-screen bg-base-100">
-            <div className="hero-content text-center">
-                <div className="max-w-md">
-                    <h1 className="text-5xl font-bold">CrowdFunding Platform</h1>
-                    <p className="py-6">description.....</p>
-                    <button className="btn">Campaigns</button>
+
+    return (
+        <main data-theme="light">
+            <div className="hero h-[70vh] bg-base-200">
+                <div className="hero-content text-center">
+                    <div className="max-w-screen-md">
+                        <h1 className="text-5xl font-bold">Welcome to AggieFunds</h1>
+                        <p className="py-6">A platform for Texas A&M students, organizations, and alumni to raise funds for their initiatives.</p>
+
+                        <a href="/Campaigns" className="btn bg-[#500000] btn-ghost text-white hover:bg-[#330b0b]"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+                            <path d="M5.433 13.917l1.262-3.155A4 4 0 017.58 9.42l6.92-6.918a2.121 2.121 0 013 3l-6.92 6.918c-.383.383-.84.685-1.343.886l-3.154 1.262a.5.5 0 01-.65-.65z" />
+                            <path d="M3.5 5.75c0-.69.56-1.25 1.25-1.25H10A.75.75 0 0010 3H4.75A2.75 2.75 0 002 5.75v9.5A2.75 2.75 0 004.75 18h9.5A2.75 2.75 0 0017 15.25V10a.75.75 0 00-1.5 0v5.25c0 .69-.56 1.25-1.25 1.25h-9.5c-.69 0-1.25-.56-1.25-1.25v-9.5z" />
+                        </svg>
+                            Start A Campaign</a>
+                    </div>
                 </div>
             </div>
-        </div>
-    </main>
-  )
+
+            <section className="text-center pt-12 mt-2">
+                <h2 className="text-4xl font-bold">Why Choose Us?</h2>
+                <p className="py-6 w-[70vh] mx-auto">We provide a secure and reliable platform for fundraising. Our mission is to empower individuals and foster direct impact within our community.</p>
+            </section>
+
+            <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 p-10 mt-0 m-5">
+                <div className="card w-96 bg-base-100 shadow-xl">
+                    <div className="card-body">
+                        <h2 className="card-title"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+                            <path fillRule="evenodd" d="M9.661 2.237a.531.531 0 01.678 0 11.947 11.947 0 007.078 2.749.5.5 0 01.479.425c.069.52.104 1.05.104 1.59 0 5.162-3.26 9.563-7.834 11.256a.48.48 0 01-.332 0C5.26 16.564 2 12.163 2 7c0-.538.035-1.069.104-1.589a.5.5 0 01.48-.425 11.947 11.947 0 007.077-2.75zm4.196 5.954a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" />
+                        </svg>
+                            Secure Platform</h2>
+                        <p>We prioritize the security of your funds and personal information.</p>
+                    </div>
+                </div>
+
+                <div className="card w-96 bg-base-100 shadow-xl">
+                    <div className="card-body">
+                        <h2 className="card-title"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+                            <path fillRule="evenodd" d="M1 4a1 1 0 011-1h16a1 1 0 011 1v8a1 1 0 01-1 1H2a1 1 0 01-1-1V4zm12 4a3 3 0 11-6 0 3 3 0 016 0zM4 9a1 1 0 100-2 1 1 0 000 2zm13-1a1 1 0 11-2 0 1 1 0 012 0zM1.75 14.5a.75.75 0 000 1.5c4.417 0 8.693.603 12.749 1.73 1.111.309 2.251-.512 2.251-1.696v-.784a.75.75 0 00-1.5 0v.784a.272.272 0 01-.35.25A49.043 49.043 0 001.75 14.5z" clipRule="evenodd" />
+                        </svg>
+                            Funding</h2>
+                        <p>All of the funds raised go directly to the campaign creators.</p>
+                    </div>
+                </div>
+
+                <div className="card w-96 bg-base-100 shadow-xl">
+                    <div className="card-body">
+                        <h2 className="card-title"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+                            <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-1.5 0a6.5 6.5 0 11-11-4.69v.447a3.5 3.5 0 001.025 2.475L8.293 10 8 10.293a1 1 0 000 1.414l1.06 1.06a1.5 1.5 0 01.44 1.061v.363a1 1 0 00.553.894l.276.139a1 1 0 001.342-.448l1.454-2.908a1.5 1.5 0 00-.281-1.731l-.772-.772a1 1 0 00-1.023-.242l-.384.128a.5.5 0 01-.606-.25l-.296-.592a.481.481 0 01.646-.646l.262.131a1 1 0 00.447.106h.188a1 1 0 00.949-1.316l-.068-.204a.5.5 0 01.149-.538l1.44-1.234A6.492 6.492 0 0116.5 10z" clipRule="evenodd" />
+                        </svg>
+                            Building Community</h2>
+                        <p>We're not just a platform, we're a community. Join us in supporting each other's dreams and initiatives.</p>
+                    </div>
+                </div>
+            </section>
+
+            <footer className="p-10 text-center">
+                <h2 className="text-2xl font-bold">Questions?</h2>
+                <p className="mt-3">We're here to help and answer any question you might have.</p>
+                <a href="/Contact" className="btn mt-5"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+                    <path fillRule="evenodd" d="M10 3c-4.31 0-8 3.033-8 7 0 2.024.978 3.825 2.499 5.085a3.478 3.478 0 01-.522 1.756.75.75 0 00.584 1.143 5.976 5.976 0 003.936-1.108c.487.082.99.124 1.503.124 4.31 0 8-3.033 8-7s-3.69-7-8-7zm0 8a1 1 0 100-2 1 1 0 000 2zm-2-1a1 1 0 11-2 0 1 1 0 012 0zm5 1a1 1 0 100-2 1 1 0 000 2z" clipRule="evenodd" />
+                </svg>
+                    Contact Us</a>
+            </footer>
+
+        </main>
+    )
 }
